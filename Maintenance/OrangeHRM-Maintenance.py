26c82a7874ba29e7df88590687b1951e91882fce
Khansa <108682140+khansa-ta@users.noreply.github.com>
2022-08-05 19:37:38 +0700

    Create OrangeHRM-Maintenance.py

--- Maintenance/OrangeHRM-Maintenance.py
+++ Maintenance/OrangeHRM-Maintenance.py
@@ -9,7 +9,6 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome import options
 
 #variables
 username = "Admin"
