ba028bfb835b652edecd4dae63b7354c3e44bbb5
Khansa <108682140+khansa-ta@users.noreply.github.com>
2022-08-05 19:34:33 +0700

    Update OrangeHRM-Leave.py

--- Leave/OrangeHRM-Leave.py
+++ Leave/OrangeHRM-Leave.py
@@ -3,7 +3,6 @@ import unittest
 import time
 from selenium import webdriver 
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
